42b1b57af536c348d9cee99e12fdc3d11de090ca
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2022-01-07 00:12:39 -0300

    Ativar e desativar estilo dependendo do valor do contador

--- src/Components/IFoodCounter/Ifoodcounter.js
+++ src/Components/IFoodCounter/Ifoodcounter.js
@@ -1,21 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./ifoodcounter.css"
 
 function Ifoodcounter() {
     const [unit, setUnit] = useState(1);
+    const [btnStyle, setBtnStyle] = useState("countex-button-minus-activated");
+
 
     function down() {
         if(unit > 0) {
-            setUnit(unit - 1)
+            setUnit(unit - 1);
+        }
+        if(unit <= 1) {
+            setBtnStyle("countex-button-minus-deactivated");
         }
     }
 
     function up() {
-        setUnit(unit + 1)    
+        setUnit(unit + 1);
+        setBtnStyle("countex-button-minus-activated");    
     }
+
     return (
         <>
-            <button className="countex-button-minus-activated"
+            <button className={btnStyle}
                  onClick={down}>-</button>
             <span>{unit}</span>
             <button className="countex-button-plus-activated"
