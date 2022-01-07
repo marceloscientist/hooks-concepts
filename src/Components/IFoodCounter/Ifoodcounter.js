786cf39cdcd3606bc20241239aab4fa94490c391
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2022-01-06 23:51:04 -0300

    adicionada um pouco de flores e separada soma e subtração em handles

--- src/Components/IFoodCounter/Ifoodcounter.js
+++ src/Components/IFoodCounter/Ifoodcounter.js
@@ -1,13 +1,25 @@
 import { useState } from "react";
+import "./ifoodcounter.css"
 
 function Ifoodcounter() {
-    const [quantity, updateQuantity] = useState(1);
+    const [unit, setUnit] = useState(1);
 
+    function down() {
+        if(unit > 0) {
+            setUnit(unit - 1)
+        }
+    }
+
+    function up() {
+        setUnit(unit + 1)    
+    }
     return (
         <>
-            <span onClick={()=>updateQuantity(quantity - 1)}>-</span>
-            <span>{quantity}</span>
-            <span onClick={()=>updateQuantity(quantity + 1)}>+</span>
+            <button className="countex-button-minus-activated"
+                 onClick={down}>-</button>
+            <span>{unit}</span>
+            <button className="countex-button-plus-activated"
+                onClick={up}>+</button>
         </>
     );
 }
